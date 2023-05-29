cf196bfe0ed38196b67efd749c56f2efb97cd567
Daki404 <daki403.1@gmail.com>
2023-05-29 20:55:51 +0900

    [Gold III] Title: 서울 지하철 2호선, Time: 272 ms, Memory: 34192 KB -BaekjoonHub

--- 백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.py
+++ 백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.py
@@ -1,33 +1,43 @@
+from collections import deque
 import sys
 
 input = sys.stdin.readline
 
 
-def dfs(field):
-    for st in range(1, n + 1):
-        log = []
-        out_log = []
-        visited = set([st])
-        stack = [(st, [st])]
+def find_cycle_node(field, st=1):
+    log = []
+    stack = [(st, [st])]
 
-        while stack:
-            node, path = stack.pop()
-            visited.add(node)
+    while stack:
+        node, path = stack.pop()
 
-            for i in field[node]:
-                if i in visited:
-                    if i == st and len(path) != 2:
-                        log += path
-                    else:
-                        out_log.append(path)
-                    continue
-
-                stack.append((i, path[:] + [i]))
-        if set(log):
-            return set(log), out_log
+        for i in field[node]:
+            if i in path:
+                if path[-2] != i:
+                    return path[(path.index(i)):]
+                continue
+            stack.append((i, path[:] + [i]))
 
 
-    
+def get_bfs_distance(field, cross_node):
+    visit = set([cross_node[0]])
+    queue = deque([(cross_node[0], 0)])
+    result = [0] * (len(field)+1)
+
+    while queue:
+        node, cost = queue.popleft()
+        result[node] = cost
+        
+        for i in field[node]:
+            if i in visit:
+                continue
+            if i in cross_node:
+                queue.append((i, 0))
+            else:
+                queue.append((i, cost + 1))
+            visit.add(i)
+    return result[1:]
+
 
 n = int(input())
 field = {i:[] for i in range(1, n+1)}
@@ -37,16 +47,7 @@ for _ in range(n):
     field[a].append(b)
     field[b].append(a)
 
-cross_node, out_node = dfs(field)
-result = [0] * (n+1)
+cross_node = find_cycle_node(field)
+result = get_bfs_distance(field, cross_node)
 
-for i in out_node:
-    if (st := i[-1]) in cross_node:
-        continue
-    
-    for j in range(len(i)-1, -1, -1):
-        if i[j] in cross_node:
-            result[st] = len(i)-j-1
-            break
-
-print(*result[1:])
+print(*result)
