0d9354a59ec45db5b100e2753a08229b88151e48
Daki404 <daki403.1@gmail.com>
2024-01-22 15:36:05 +0900

    [Gold II] Title: 기업투자, Time: 276 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2662. 기업투자/기업투자.py
+++ 백준/Gold/2662. 기업투자/기업투자.py
@@ -1,49 +1,31 @@
 import sys; input = sys.stdin.readline
-from typing import List
-
-
-def invest(company_idx: int, capacity: int) -> int:
-    if company_idx == m\
-            or company_idx < 0:
-        return 0
-    
-    if (ret := cache[company_idx][capacity]):
-        return ret
-    
-    ret = invest(company_idx+1, capacity)
-
-    for money in range(1, capacity+1):
-        ret = max(ret,
-                  invest(company_idx+1, capacity-money)
-                  + company_invest[money-1][company_idx])
-    
-    cache[company_idx][capacity] = ret
-    return ret
-
-
-def reconstruct(company_idx: int, capacity: int) -> None:
-    if company_idx == m:
-        return
-    
-    if invest(company_idx, capacity) == invest(company_idx+1, capacity):
-        picked.append(0)
-        reconstruct(company_idx+1, capacity)
-    
-    else:
-        for i in range(1, capacity+1):
-            if (company_invest[i-1][company_idx] + invest(company_idx+1, capacity-i))\
-                    == invest(company_idx, capacity):
-                picked.append(i)
-                reconstruct(company_idx+1, capacity-i)
-                break
 
 
 n, m = map(int, input().split())
+values = [[0] * (n+2)]
 
-picked = []
-cache = [[0] * (n+1) for _ in range(m)]
-company_invest = [list(map(int, input().split()))[1:] for _ in range(n)]
+for _ in range(n):
+    line = list(map(int, input().split()))
+    values.append(line)
 
-print(invest(0, n))
-reconstruct(0, n)
-print(*picked)
+
+dp = [[0] * (n+2) for _ in range(m+2)]
+path = [[0] * (n+2) for _ in range(m+2)]
+
+for i in range(1, m+1):
+    for j in range(1, n+1):
+        dp[i][j] = dp[i-1][j]
+
+        for k in range(1, j+1):
+            if dp[i][j] < (tmp := values[k][i] + dp[i-1][j-k]):
+                dp[i][j] = tmp
+                path[i][j] = k
+
+print(dp[m][n])
+result = []
+for i in range(m, 0, -1):
+    picked = path[i][n] 
+    result.append(picked)
+    n -= picked
+
+print(*result[::-1])
