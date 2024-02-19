18cb9e7e0a36820cb9db7a14daf2754c2c4054ab
Daki404 <daki403.1@gmail.com>
2024-02-19 15:36:59 +0900

    [level 2] Title: 가장 큰 수, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,5 +1,13 @@
+from functools import cmp_to_key
+
 def solution(numbers):
+    def compare(a, b):
+        t1 = int(a+b)
+        t2 = int(b+a)
+        
+        return (t1 > t2) - (t1 < t2)
+    
     numbers = list(map(str, numbers))
-    numbers.sort(reverse = True, key= lambda x: x*4)
+    numbers.sort(reverse=True, key=cmp_to_key(compare))
     
     return str(int(''.join(numbers)))
